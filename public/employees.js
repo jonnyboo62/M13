93350566ed0915148262dd652c567c3a388e4b3f
jonnyboo62 <101075746+jonnyboo62@users.noreply.github.com>
2024-04-22 14:05:29 -0700

    Add Code

---
+++ public/employees.js
@@ -0,0 +1,7 @@
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    'color': 'blue',
+    'fontSize': '24px'
+  }
+}, /*#__PURE__*/React.createElement("li", null, "Sally Smith"), /*#__PURE__*/React.createElement("li", null, "Mark Martin"), /*#__PURE__*/React.createElement("li", null, "Holly Unlikely"));
+ReactDOM.render(element, document.getElementById('content'));
---
+++ public/employees.jsx
@@ -0,0 +1,9 @@
+const element = (
+    <ul style={{'color':'blue', 'fontSize': '24px'}}>
+        <li>Sally Smith</li>
+        <li>Mark Martin</li>
+        <li>Holly Unlikely</li>
+        <li>Smith Jon</li>
+    </ul>
+)
+ReactDOM.render(element, document.getElementById('content'))
